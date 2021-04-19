732d66a9c9601c5663acc0675c376b934fba9793
Zmccurtain <zmccurtain2@gmail.com>
2021-04-18 20:30:56 -0400

    Added role preference to rosters and fixed elo and roster methods to accomodate this.also added the base class for the balancing of teams

--- Roster.py
+++ Roster.py
@@ -1,16 +1,21 @@
+#Some of this code existed previously, but has been overhauled 
+#massively to the point where it is basically new code.
+import re
+
 def Edit(target, new):
     if target == "c":
         x = open("Current.txt", "w")
     if target == "l":
         x = open("Last.txt", "w")
     for i in new:
-        x.write(i + ":" + str(new[i])+"\n")
+        roles = "["
+        for role in new[i][1]:
+            roles += str(role) + ","
+        roles = roles[:-1]+"]"
+        x.write("{}:{}:{}\n".format(i, new[i][0], roles))
     x.close()
-    if target=="c":
-        print(oRoster("c"))
 def oRoster(target):
     if target == "c":
-    
         x = open("Current.txt", "r")
     elif target == "d":
         x = open("Default.txt", "r")
@@ -19,8 +24,16 @@ def oRoster(target):
     lines = x.readlines()
     roster = {}
     for line in lines:
-        i,j = line.split(":")
-        roster[i] = float(j.split("\n")[0])
+        line = line.strip()
+        try:
+            i,j,z = line.split(":")
+        except:
+            print(line.split(":"))
+        roles = []
+        for role in re.sub("(\[|\])","",z).split(","):
+            roles.append(int(role))
+        roster[i] = [float(j), roles]
+        
     return roster
 
 def Reset():
